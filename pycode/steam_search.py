56d025673a5ba11997a0d674ab43c50e51fa246b
huh <ivangalaktionov24@gmail.com>
2026-06-23 23:02:37 +0300

    Little update

--- pycode/steam_search.py
+++ pycode/steam_search.py
@@ -15,9 +15,29 @@ class SteamWindow(QWidget):
 
         self.setWindowIcon(QIcon('resources/Logo 3.png'))
         self.setWindowTitle('SteamSearch')
-        
+
         menu = QMenu()
         menu.addAction(self.actionSteamId)
         menu.addAction(self.actionAppId)
         menu.addAction(self.actionNews)
+
         self.otherFunctionsBtn.setMenu(menu)
+        self.back_btn = QPushButton('<-', self)
+        self.back_btn.setStyleSheet("""
+            QPushButton {
+                background-color: rgba(217, 217, 217, 1);
+                color: #333;
+                text-decoration: none;
+                border-radius: 10px;
+                font-weight: 900;
+                font-size: 24px;
+            }
+        """)
+
+        self.back_btn.resize(40, 20)
+        self.back_btn.clicked.connect(self.back)
+
+    def back(self):
+        self.last_window.show()
+        hp.center_window(self.last_window)
+        self.close()
